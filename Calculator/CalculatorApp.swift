fd6f7ee713a51caa9c5aa4cad37aa4546318d232
Sai Lakshmi <sailakshmi22.p@gmail.com>
2022-09-21 18:35:05 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Sai Lakshmi on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
