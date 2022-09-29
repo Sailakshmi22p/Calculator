70501123522fca1458f89db5932daace3bc40fa1
Sai Lakshmi <sailakshmi22.p@gmail.com>
2022-09-29 12:13:31 -0400

    New files

--- Calculator/Views/ContentView.swift
+++ Calculator/Views/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.pink.opacity(0.3), .purple.opacity(0.5)] ), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            KeyView()
+        }
     }
 }
 
